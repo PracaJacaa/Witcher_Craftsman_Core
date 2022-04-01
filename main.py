6bedb6501e547a500b10c0f087c20bc12c7b7eb4
Jacek Gipsiak <Gipsiak77@gmail.com>
2022-04-02 00:00:30 +0200

    Now its with csv file not a txt

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import PySimpleGUI as pg
+import csv
+f=open("dane.csv","r")
+file = csv.DictReader(f)
 
-names = []
+Name=[]
+CraftingDc=[]
+Time=[]
+Timber=[]
+Iron=[]
+Fethers=[]
+Hardened_Leather=[]
+Steel=[]
+Investmend=[]
+Cost =[]
 
-f = open("names.txt","r")
-for line in f:
-    line=line.replace("\n","")
-    names.append(line)
+for col in file:
+    Name.append(col["Name"])
+    CraftingDc.append(col["Crafting dc"])
+    Time.append(col["Timber"])
+    Fethers.append(col["Fethers"])
+    Hardened_Leather.append(col["Hardened Leather"])
+    Steel.append(col["Steel"])
+    Investmend.append(col["Investmend"])
+    Cost.append(col["Cost"])
 
-f.close()
+
+###NO MORE DATA DOWN 
 
 pg.theme("darkAmber")
 layout=[
@@ -22,7 +40,7 @@ while True:
     if event == "cancel" or event ==pg.WIN_CLOSED:
         break
     print(values)
-    print(names)
+    print(Name)
 
 
 window.close()
